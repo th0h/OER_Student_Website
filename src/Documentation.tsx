93c21d6d994b320afc84b2626a0a4c2c6396abe2
th0h <133070274+th0h@users.noreply.github.com>
2024-11-22 16:06:55 -0500

    sidebar into doc

--- src/Documentation.tsx
+++ src/Documentation.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import data from '../src/data/doc_data.json';
 import Section from '../src/components/Section'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSideBar from "@/components/app-sidebar";
 
 const Documentation: React.FC = () => {
   const { doc_body, sections } = data;
 
   return (
+    <SidebarProvider>
+              <AppSideBar />
+              <SidebarTrigger />
     <section className="p-2 tablet:p-4 laptop:p-10 rounded-lg max-w-screen-xl mx-auto text-center tablet:text-left">
       <div id="doc_body">
         <h1>{doc_body.title}</h1>
@@ -21,6 +26,7 @@ const Documentation: React.FC = () => {
         ))}
       </div>
     </section>
+    </SidebarProvider>
   );
 };
 
