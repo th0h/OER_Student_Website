d2b97b8d5f1921f8392edeacd1b1bd96b27c3141
th0h <133070274+th0h@users.noreply.github.com>
2024-11-22 16:16:13 -0500

    IDK -t

---
+++ src/Documentation.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import data from '../src/data/doc_data.json';
+import Section from '../src/components/Section'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSideBar from "@/components/app-sidebar";
+
+const Documentation: React.FC = () => {
+  const { doc_body, sections } = data;
+
+  return (
+    <SidebarProvider>
+              <AppSideBar />
+              <SidebarTrigger />
+    <section className="p-2 tablet:p-4 laptop:p-10 rounded-lg max-w-screen-xl mx-auto text-center tablet:text-left">
+      <div id="doc_body">
+        <h1>{doc_body.title}</h1>
+        <p id="doc_intro">{doc_body.intro}</p>
+        <br />
+        {sections.map((section) => (
+          <Section
+            key={section.id}
+            id={section.id}
+            title={section.title}
+            content={section.content}
+          />
+        ))}
+      </div>
+    </section>
+    </SidebarProvider>
+  );
+};
+
+export default Documentation;
